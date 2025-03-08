c02fbb0e2378190744031c38cfeb6ad020c6ff48
Pradnya Kulkarni <157954530+PradnyaKulkarni2005@users.noreply.github.com>
2025-03-08 12:48:27 +0530

    Update api.js

--- Frontend/herworld/src/api.js
+++ Frontend/herworld/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:5001/api"; // Ensure this matches your backend port
+const API_URL = const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5001/api"; // Fallback to localhost if no environment variable is set
+
 
 // Register User
 export const registerUser = async (userData) => {
